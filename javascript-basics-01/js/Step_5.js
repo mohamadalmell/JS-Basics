6f59cfa708f6b699d194b16cc466681907e66cf1
Mohamad <mohamadalmell@gmail.com>
2021-11-24 21:26:36 +0200

    Simple Math

---
+++ javascript-basics-01/js/Step_5.js
@@ -0,0 +1,6 @@
+document.getElementById('validate').addEventListener("click", function() {
+    var num1 = document.getElementById('first_number').value;
+    var num2 = document.getElementById('second_number').value;
+
+    alert(num1*num2);
+});
