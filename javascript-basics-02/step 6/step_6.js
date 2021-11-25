aac4622652d6820d3b578548096981a0592d0326
Mohamad <mohamadalmell@gmail.com>
2021-11-25 19:36:01 +0200

    JS-02 Step 6

---
+++ javascript-basics-02/step 6/step_6.js
@@ -0,0 +1,11 @@
+var images = document.querySelectorAll('#image1, #image2, #image3, #image4, #image5');
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("mouseover",function () {
+        this.src = "images/image" + (i+1) +"_2.jpg";
+    })
+
+    images[i].addEventListener("mouseout",function () {
+        this.src = "images/image" + (i+1)  +".jpg";
+    })
+}
