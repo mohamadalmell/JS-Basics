ab2e60da92f17a9a3a64e8d2430038b817374471
Mohamad <mohamadalmell@gmail.com>
2021-11-25 19:30:57 +0200

    JS-02 Step 5

--- javascript-basics-02/step 5/step_5.js
+++ javascript-basics-02/step 5/step_5.js
@@ -1,4 +1,7 @@
-var x = document.querySelector("img");
-x.addEventListener("mouseover", () => {
-    x.style.transform = "translateY(50px)";
-})
+var images = document.querySelectorAll('#image1, #image2, #image3, #image4, #image5');
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("mouseover",function () {
+        this.src = "images/image" + (i+1) +"_2.jpg";
+    }) 
+}
